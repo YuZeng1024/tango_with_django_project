68bca40cdae421fbb0cb3eefe9a6bae58610cef9
Yu Zeng <2543042z@student.gla.ac.uk>
2022-02-22 02:15:45 +0000

    user controll

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -94,6 +94,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS':{ 'min_length':6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -126,3 +127,5 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+
+LOGIN_URL = 'rango:login'
